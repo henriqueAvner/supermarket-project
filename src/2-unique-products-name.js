6d16ee46e00acee5a2745ac631c1c1eed3b90f03
henriqueAvner <avnerhdpb@gmail.com>
2023-06-07 12:44:57 -0300

    Requisito 2 -nomes dos produtos em array

--- src/2-unique-products-name.js
+++ src/2-unique-products-name.js
@@ -1,7 +1,12 @@
 const stockProducts = require('./data.json');
 
 const getUniqueProductsName = () => {
-  // Desenvolva seu código dentro dessa função...
+  let nameProducts = [];
+  for (let index = 0; index < stockProducts.length; index += 1) {
+     nameProducts.push(stockProducts[index].productName);
+  }
+
+  return nameProducts;
 };
 
 module.exports = { getUniqueProductsName };
