f5f2c87526c9130c1f5f743772d79056a9977f50
henriqueAvner <avnerhdpb@gmail.com>
2023-06-07 16:18:50 -0300

    Requisito 5- retornando a soma de todos os produtos em estoque

--- src/5-products-amount.js
+++ src/5-products-amount.js
@@ -1,7 +1,13 @@
 const stockProducts = require('./data.json');
 
 const getProductsAmount = () => {
-  // Desenvolva seu código dentro dessa função...
+  let sumProducts = 0;
+  for( let index = 0; index <stockProducts.length; index += 1){
+    if(stockProducts[index].quantityInStock > 0){
+      sumProducts += stockProducts[index].quantityInStock;
+    }
+  }
+  return sumProducts;
 };
 
 module.exports = { getProductsAmount };
