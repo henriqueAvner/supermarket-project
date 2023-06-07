3261073591808cd6774748d9e49cb2989e0c0b3e
henriqueAvner <avnerhdpb@gmail.com>
2023-06-07 14:15:43 -0300

    Requisito 3- Produtos indisponiveis no estoque

--- src/3-out-of-stock-products.js
+++ src/3-out-of-stock-products.js
@@ -1,7 +1,13 @@
 const stockProducts = require('./data.json');
 
 const getOutOfStockProducts = () => {
-  // Desenvolva seu código dentro dessa função...
+  let indispProducts =[];
+  for(index = 0; index < stockProducts.length; index += 1 ) {
+    if(stockProducts[index].quantityInStock == 0){
+      indispProducts.push(stockProducts[index].productName)
+    }
+  }
+  return indispProducts;
 };
 
 module.exports = { getOutOfStockProducts };
