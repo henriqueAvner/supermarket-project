9f3ba7097d3757f0af8862ad2f4b36f3c4842951
henriqueAvner <avnerhdpb@gmail.com>
2023-06-07 15:00:03 -0300

    Requisito 4- Produtos em baixo estoque

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -1,7 +1,19 @@
 const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
-  // Desenvolva seu código dentro dessa função...
+  let lowStock = [];
+  
+  for(let index = 0; index < stockProducts.length; index += 1){
+    if(stockProducts[index].quantityInStock > 0 && stockProducts[index].quantityInStock <= 10){
+      lowStock.push(
+        `${stockProducts[index].productName}: ${stockProducts[index].quantityInStock} unidades`
+      );
+      
+   } 
+
+  }
+  
+  return lowStock;
 };
 
 module.exports = { getLowStockProducts };
