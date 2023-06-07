a1c0f7728e28464d6ae465fbfa53ce0cc1e87b2a
henriqueAvner <avnerhdpb@gmail.com>
2023-06-07 19:37:38 -0300

    Requisito 8 - adicionando produtos em oferta

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -1,7 +1,19 @@
 const stockProducts = require('./data.json');
 
 const getProductsOnSale = () => {
-  // Desenvolva seu código dentro dessa função...
+  let productsArray = [];
+  
+  for(let index = 0; index < stockProducts.length; index += 1){
+    let prOnSale = {};
+    if(stockProducts[index].onSale === true){
+      prOnSale.description = stockProducts[index].description;
+      prOnSale.formattedPrice = `R$ ${stockProducts[index].price.toFixed(2)}`;
+      prOnSale.onSale = stockProducts[index].onSale;
+      
+      productsArray.push(prOnSale);
+    }
+  }
+  return productsArray;
 };
 
 module.exports = { getProductsOnSale };
