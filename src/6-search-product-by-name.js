081e32f2407307726f110f8644c39dfe2c3b5664
henriqueAvner <avnerhdpb@gmail.com>
2023-06-07 18:54:02 -0300

    Resolvendo requisito 7, e retirando variáveis desnecessárias

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -2,11 +2,9 @@ const stockProducts = require('./data.json');
 
 const searchProductByName = (nameProduct) => {
   const infoProduct = {};
-  let productExists = false;
   
   for(let index = 0; index < stockProducts.length; index += 1){
     if(stockProducts[index].productName === nameProduct){
-      productExists = true;
       infoProduct.description = `${stockProducts[index].description}`;
       infoProduct.formattedPrice = `R$ ${stockProducts[index].price}`;
       return infoProduct;
--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,20 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
-  // Desenvolva seu código dentro dessa função...
-};
+const searchProductsByBrand = (brand) => {
+  const infoProduct = [];
+  
+  for(let index = 0; index < stockProducts.length; index += 1){
+    if(stockProducts[index].brand === brand){
+  
+      infoProduct.push({
+        description: `${stockProducts[index].description}`,
+        formattedPrice: `R$ ${stockProducts[index].price}`
+      })
+    } 
+    
+    }
+    return infoProduct;
+    
+  }
 
 module.exports = { searchProductsByBrand };
