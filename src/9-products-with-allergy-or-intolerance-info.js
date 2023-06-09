1ca8cf1bfd6e0524ed405c539355414ebbc5f3ef
henriqueAvner <avnerhdpb@gmail.com>
2023-06-09 14:45:33 -0300

    Requisito 9 - Produtos de alergia ou intolerância

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -1,7 +1,19 @@
 const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
-  // Desenvolva seu código dentro dessa função...
+ let alergicProducts = [];
+  for(let index = 0; index < stockProducts.length; index += 1){
+    let objProducts = {};
+    if(stockProducts[index].allergyOrIntolerance !== undefined){
+      objProducts.description = stockProducts[index].description;
+      objProducts.formattedPrice = `R$ ${stockProducts[index].price}`;
+      objProducts.allergyOrIntoleranceMessage = `Pode conter: ${stockProducts[index].allergyOrIntolerance.join(' ')}`;
+
+      alergicProducts.push(objProducts);
+    }
+
+  }
+  return alergicProducts;
 };
 
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
